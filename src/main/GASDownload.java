7003da9a0dacaf780ca4ee85fd4bac9cfab379d2
mgmg-zbtn <takeshita6623@gmail.com>
2022-07-20 00:36:00 +0900

    rename and readme

--- src/main/GASDownload.java
+++ src/main/GASDownload.java
@@ -11,22 +11,8 @@ import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 
 import com.google.gson.Gson;
-/**
- * Google Drive API は面倒なのでブラウザでjsonダウンロードまでは手動
- * ダウンロードしたファイルをもとに、ディレクトリを作ってGASプロジェクトを再構成する感じ
- * 
- * 自動化したかったけどかかる労力に見合わない
- * GAS自体スプレッドシートと紐づけてしか動かさないし動かせない
- * 元々作ったものをgithubにまとめるためにやってたやつ
- * 
- * 
- * ID = "ur google app script project id";
- * https://script.google.com/feeds/download/export?id=" + ID + "&format=json
- * json file download and set root directory
- * 
- * @since 2022/07/12
- *
- */
+
+
 public class GASDownload {
 
 	public static void main(String[] args) throws
